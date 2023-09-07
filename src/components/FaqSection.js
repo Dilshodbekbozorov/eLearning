15723577277dcbbcebfc0c506d729e6c6350ec34
Dilshodbek <bozorovdilshodbekh@gmail.com>
2023-09-07 19:24:44 +0500

    FaqSection style completed

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,7 +1,10 @@
 import React from "react";
+// styles
+import styled from "styled-components"
+import { About } from '../style'
 
 const FaqSection = () => {
-  return <div className="faq">
+  return <Faq>
     <h2>
       Any Questions <span>FAQ</span>
     </h2>
@@ -13,6 +16,7 @@ const FaqSection = () => {
         <p>Lorem ipsum dolor sit amet.</p>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, voluptas.</p>
       </div>
+      <div className="faq-line"></div>
     </div>
     <div className="question">
       <h4>How much do I have to pay per month?</h4>
@@ -20,6 +24,7 @@ const FaqSection = () => {
         <p>Lorem ipsum dolor sit amet.</p>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, voluptas.</p>
       </div>
+      <div className="faq-line"></div>
     </div>
     <div className="question">
       <h4>Is it possible to pay in full in one go? </h4>
@@ -27,6 +32,7 @@ const FaqSection = () => {
         <p>Lorem ipsum dolor sit amet.</p>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, voluptas.</p>
       </div>
+      <div className="faq-line"></div>
     </div>
     <div className="question">
       <h4>If I buy the course, can my brother and I also study? </h4>
@@ -34,8 +40,36 @@ const FaqSection = () => {
         <p>Lorem ipsum dolor sit amet.</p>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, voluptas.</p>
       </div>
+      <div className="faq-line"></div>
     </div>
-  </div>
+  </Faq>
 }
+
+const Faq = styled(About)`
+  display: block;
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+
+  .answer {
+    padding: 2rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+
+  .faq-line {
+    background-color: #ccc;
+    height: 0.2rem;
+    width: 100%;
+    margin: 2rem 0rem;
+  }
+`;
 
 export default FaqSection
